5849561cceb366029c513532e5b54010a756491d
geethaperuru <41207848+geethaperuru@users.noreply.github.com>
2020-02-06 23:57:52 +0530

    Eslint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,20 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/react-app"],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["erroe", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"]
   }
 };
